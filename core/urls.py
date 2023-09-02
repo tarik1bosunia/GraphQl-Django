a9862078e8b8afbabe30de557006b24839410687
md tarik bosunia <bosuniamdtarik005@gmail.com>
2023-09-02 11:09:24 +0600

    project set up and models created

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for cookbook project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,11 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from graphene_django.views import GraphQLView
-from django.views.decorators.csrf import csrf_exempt
-from .schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
--- ingredients/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from ingredients.models import Category, Ingredient
-
-admin.site.register(Category)
-admin.site.register(Ingredient)
-
